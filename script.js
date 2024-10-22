44214c5849858e2ddd17236c14a3616df5d43976
Bayumilhan <bayumilhan56@gmail.com>
2024-10-22 17:17:22 +0700

    Attempt 1 - 10.30

--- script.js
+++ script.js
@@ -12,35 +12,37 @@ const generatePassword = (length, options) => {
     let password= "";
 
    
-   if (options.includeUppercase) {
-       dictionary += uppercase;
-       password += uppercase[Math.floor(Math.random() * uppercase.length)];
-   }    
-   if (options.includeLowercase) {
-       dictionary += lowercase;
-       password += lowercase[Math.floor(Math.random() * lowercase.length)];
-   } 
-   if (options.includeNumbers) {
-       dictionary += numbers;
-       password += numbers[Math.floor(Math.random() * numbers.length)];
-   }
-   if (options.includeSpecialChars) {
-       dictionary += specialChars;
-       password += specialChars[Math.floor(Math.random() * specialChars.length)];
-   }
+    if (options.includeUppercase) {
+        dictionary += uppercase;
+        password += uppercase[Math.floor(Math.random() * uppercase.length)]; 
+    }
+    if (options.includeLowercase) {
+        dictionary += lowercase;
+        password += lowercase[Math.floor(Math.random() * lowercase.length)]; 
+    }
+    if (options.includeNumbers) {
+        dictionary += numbers;
+        password += numbers[Math.floor(Math.random() * numbers.length)]; 
+    }
+    if (options.includeSpecialChars) {
+        dictionary += specialChars;
+        password += specialChars[Math.floor(Math.random() * specialChars.length)]; 
+    }
 
-   if(dictionary ==="") { 
-    throw new Error("At least one character type must be selected");
-   }
-   
-   for (let i = password.length; i<length; i++) {
-       const randomIndex = Math.floor(Math.random() * dictionary.length);
-       password += dictionary[randomIndex];
-   }
-   
-   password = password.split('').sort(() => Math.random() - 0,5).join('');
+    if (dictionary === "") {
+        throw new Error("At least one character type must be selected.");
+    }        
 
-   return password;
+    
+    for (let i = password.length; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * dictionary.length);
+        password += dictionary[randomIndex];
+    }
+
+   
+    password = password.split('').sort(() => Math.random() - 0.5).join('');
+    
+    return password;
 };
 
 module.exports = {
