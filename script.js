058371b6c49b039bfe838e7df04ce508ec4084b1
Bayumilhan <bayumilhan56@gmail.com>
2024-10-22 16:51:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,24 +8,39 @@ const generatePassword = (length, options) => {
     const specialChars = "!@#$%^&*()";
 
     // TODO: Create a variable for the character set based on selected options
-    let charSet= '';
+   let dictionary = "";
+    let password= "";
 
-   // TODO: Generate the password based on the selected criteria
-   if (options.includeUppercase) charSet += uppercase;
-   if (options.includeLowercase) charSet += lowercase;
-   if (options.includeNumbers) charSet += numbers;
-   if (options.includeSpecialChars) charSet += specialChars;
+   
+   if (options.includeUppercase) {
+       dictionary += uppercase;
+       password += uppercase[Math.floor(Math.random() * uppercase.length)];
+   }    
+   if (options.includeLowercase) {
+       dictionary += lowercase;
+       password += lowercase[Math.floor(Math.random() * lowercase.length)];
+   } 
+   if (options.includeNumbers) {
+       dictionary += numbers;
+       password += numbers[Math.floor(Math.random() * numbers.length)];
+   }
+   if (options.includeSpecialChars) {
+       dictionary += specialChars;
+       password += specialChars[Math.floor(Math.random() * specialChars.length)];
+   }
 
-   if(charSet ===''){
-     throw new Error('Please select at least one option for the password.');  
+   if(dictionary ==="") { 
+    throw new Error("Please select at least one option for the password..");
    }
    
-   let password ='';
-   for (let i = 0; i<length; i++) {
-       const randomIndex = Math.floor(Math.random() * charSet.length);
-       password += charSet[randomIndex];
+   for (let i = password.length; i<length; i++) {
+       const randomIndex = Math.floor(Math.random() * dictionary.length);
+       password += dictionary[randomIndex];
    }
    
+   password = password.split('').sort(() => Math.random() - 0,5).join('');
+
    return password;
 };
+
 
